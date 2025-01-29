21cf1f18949fab595b4ed6119d05068b616d77ff
julesgrrd <jules.girardon@edu.devinci.fr>
2025-01-29 13:14:01 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Projet_info
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
