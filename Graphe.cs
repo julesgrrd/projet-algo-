7f756cd800a468003e686bd7418d81c2a818bb53
julesgrrd <jules.girardon@edu.devinci.fr>
2025-02-06 10:40:08 +0100

    Update Graphe.cs

--- Graphe.cs
+++ Graphe.cs
@@ -12,7 +12,17 @@ namespace Projet_info
 
         public int[,] generematricerelation()
         {
-            
+                string[] infoligne = tab.Skip(24).ToArray();
+                int[,] matrice_relation = new int[infoligne.Length, 2];
+                for (int i = 0; i < infoligne.Length; i++)
+                {
+                    string[] numbers = infoligne[i].Split(' ');
+                    matrice_relation[i, 0] = Convert.ToInt32(numbers[0]);
+                    matrice_relation[i, 1] = Convert.ToInt32(numbers[1]);
+                }
+            return matrice_relation;
         }
+
+        public int[,] generematriceadjacente()
      }
 }
