326d00089a6ae4afa64fe5678715146c92e27183
julesgrrd <jules.girardon@edu.devinci.fr>
2025-02-06 10:59:30 +0100

    Update Graphe.cs

--- Graphe.cs
+++ Graphe.cs
@@ -8,9 +8,17 @@ namespace Projet_info
 {
     internal class Graphe
     {
+        public int[,] matrice_relation = Generematricerlation();
+        public int[,] matrice_adjacence; 
+
+        Public Graphe( int[,] matrice_relation, int[,] matrice_adjacence ) 
+        {
+            this.matrice_relation = matrice_relation; 
+            this.matrice_adjacence = matrice_adjacence; 
+        }
 
 
-        public int[,] generematricerelation()
+        public int[,] Generematricerelation()
         {
                 string[] infoligne = tab.Skip(24).ToArray();
                 int[,] matrice_relation = new int[infoligne.Length, 2];
