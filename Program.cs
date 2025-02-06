8fdeb8ce7a8a63fe5d62fbcdbf78477c79d4e589
julesgrrd <jules.girardon@edu.devinci.fr>
2025-02-06 09:14:06 +0100

    remplir la matrice
    A partir du fichier et de la ligne 24, on remplit une matrice avec les différentes relations.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "soc-karate.mtx";
+
+        // Lire toutes les lignes du fichier
+        string[] lines = File.ReadAllLines(filePath);
+
+        // Ignorer les 23 premières lignes et stocker les données à partir de la ligne 24
+        var dataLines = lines.Skip(23).ToArray();
+
+        // Initialiser la matrice avec le bon nombre de lignes
+        int[,] matrix = new int[dataLines.Length, 2];
+
+        // Remplir la matrice
+        for (int i = 0; i < dataLines.Length; i++)
+        {
+            string[] numbers = dataLines[i].Split(' ');
+            matrix[i, 0] = int.Parse(numbers[0]);  // Première colonne
+            matrix[i, 1] = int.Parse(numbers[1]);  // Deuxième colonne
+        }
+
+        // Afficher la matrice pour vérification
+        Console.WriteLine("Matrice extraite du fichier:");
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            Console.WriteLine($"{matrix[i, 0]} {matrix[i, 1]}");
+        }
+    }
+}
+
         }
     }
 }
