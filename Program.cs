5c0dfea18760d39eba230f7a2bb45855beb2dbe4
julesgrrd <jules.girardon@edu.devinci.fr>
2025-02-06 10:25:01 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,33 +1,28 @@
 ﻿namespace Projet_info
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
     internal class Program
     {
         static void Main(string[] args)
         {
-            using System;
-using System.IO;
-using System.Linq;
-
-class Program
-{
-    static void Main()
-    {
-        string fichier = "C:\\Users\\alexi\\OneDrive - De Vinci\\ESILV\\A2\\Problème scientifique informatique\\Compléments utiles pour le livrable 1  1er Mars-20250129\\soc-karate\\soc-karate.mtx";
-        string[] tab = File.ReadAllLines(fichier);
-        string[] infoligne = tab.Skip(24).ToArray();
-        int[,] matrice_relation = new int[infoligne.Length, 2];
-        for (int i = 0; i < infoligne.Length; i++)
-        {
-            string[] numbers = infoligne[i].Split(' ');
-            matrice_relation[i, 0] = Convert.ToInt32(numbers[0]);  
-            matrice_relation[i, 1] = Convert.ToInt32(numbers[1]);  
-        }
-
-
-        
-    }
-}
-
+           
+                string fichier = "C:\\Users\\Jules\\OneDrive - De Vinci\\Documents\\Année 2\\Semestre 2\\info\\Projet info\\Association-soc-karate\\soc-karate.mtx";
+                string[] tab = File.ReadAllLines(fichier);
+                string[] infoligne = tab.Skip(24).ToArray();
+                int[,] matrice_relation = new int[infoligne.Length, 2];
+                for (int i = 0; i < infoligne.Length; i++)
+                {
+                    string[] numbers = infoligne[i].Split(' ');
+                    matrice_relation[i, 0] = Convert.ToInt32(numbers[0]);
+                    matrice_relation[i, 1] = Convert.ToInt32(numbers[1]);
+                }
+                Console.WriteLine("Matrice extraite du fichier:");
+                for (int i = 0; i < matrice_relation.GetLength(0); i++)
+                {
+                    Console.WriteLine($"{matrice_relation[i, 0]} {matrice_relation[i, 1]}");
+                }
+            }
         }
     }
-}
