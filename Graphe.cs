cea0c7a3c9d62852e10e3e51f6b68249e5756439
Alexiefrn <alexie.fournie@gmail.com>
2025-03-25 23:07:42 +0100

    Version Générique Graphe.cs

--- Graphe.cs
+++ Graphe.cs
@@ -9,28 +9,27 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace Problème_scientifique_informatique
+namespace ProjetPSI
 {
-    public class Graphe
+    public class Graphe <T>
     {
-        public int[,] matrice_relation;
-       
-        public Graphe(int[,] matrice_relation)
+        public T[,] matrice_relation { get; set; }
+
+        public Graphe(T[,] matrice_relation)
         {
             this.matrice_relation = matrice_relation;
         }
 
-
-        public int OrdreDuGraphe(int[,] matrice_relation)         /// L'ordre du graphe est le nombre de sommet qu'il contient
+        public int OrdreDuGraphe(T[,] matrice_relation)         /// L'ordre du graphe est le nombre de sommet qu'il contient
         {
             int ordre = 0;
-            for(int i=0; i<matrice_relation.GetLength(0); i++)
+            for (int i = 0; i < matrice_relation.GetLength(0); i++)
             {
-                for (int j=0; j<matrice_relation.GetLength(1); j++)
+                for (int j = 0; j < matrice_relation.GetLength(1); j++)
                 {
-                    if (matrice_relation[i,j]>ordre)                        
+                    if (Convert.ToInt32(matrice_relation[i, j]) > ordre)
                     {
-                        ordre=matrice_relation [i,j];               /// l'ordre sera donc le nombre le plus élevé de la matrice_relation, extraite du fichier
+                        ordre = Convert.ToInt32(matrice_relation[i, j]);               /// l'ordre sera donc le nombre le plus élevé de la matrice_relation, extraite du fichier
                     }
                 }
             }
@@ -38,28 +37,28 @@ namespace Problème_scientifique_informatique
             return ordre;
         }
 
-        public int TailleDuGraphe(int[,] matrice_relation)     /// La taille du graphe est le nombre d'arêtes qu'il contient soit le nombre de lignes de la matrice_relation
+        public int TailleDuGraphe(T[,] matrice_relation)     /// La taille du graphe est le nombre d'arêtes qu'il contient soit le nombre de lignes de la matrice_relation
         {
-            int taille = matrice_relation.GetLength(0);          
+            int taille = matrice_relation.GetLength(0);
             Console.WriteLine("La taille du graphe est : " + taille + ". Il y a donc " + taille + " arêtes.");
             return taille;
-            
+
         }
 
         public bool GrapheOriente(List<int>[] ListeAdjacence)           ///On souhaite vérifier si le graphe est orienté ou non
         {
             int a;
             bool result = true;
-            for (int i=0; i<ListeAdjacence.Length; i++)
+            for (int i = 0; i < ListeAdjacence.Length; i++)
             {
-                if (result==false)
+                if (result == false)
                 {
                     break;
                 }
-                for (int j=0; j < ListeAdjacence[i].Count; j++)       /// On parcours la liste d'adjacence d'un sommet
+                for (int j = 0; j < ListeAdjacence[i].Count; j++)       /// On parcours la liste d'adjacence d'un sommet
                 {
                     a = ListeAdjacence[i][j];                         /// On crée une variable contenant un sommet voisin
-                    if (ListeAdjacence[a - 1].Contains(i+1)==true)    /// On vérifie que la liste adjacence du sommet voisin contient le sommet inital
+                    if (ListeAdjacence[a - 1].Contains(i + 1) == true)    /// On vérifie que la liste adjacence du sommet voisin contient le sommet inital
                     {
                         continue;                                     /// Si oui, on continue et on vérifie pour tous les sommets de toutes les listes
                     }
@@ -70,7 +69,7 @@ namespace Problème_scientifique_informatique
                     }
                 }
             }
-            if(result==true)
+            if (result == true)
             {
                 Console.WriteLine("Le graphe est non-orienté : les relations sont réciproques");
             }
@@ -86,20 +85,20 @@ namespace Problème_scientifique_informatique
 
 
 
-        public List<int>[] GenererListeAdjacence(int[,] matrice_relation, int ordre)
-        {           
+        public List<int>[] GenererListeAdjacence(T[,] matrice_relation, int ordre)
+        {
             List<int>[] ListeAdjacence = new List<int>[ordre];       /// On crée un tableau de liste ou le nombre d'éléments du tableau sera l'ordre du graphe et à chaque éléments on associe une liste (liste de voisins)
 
-            for (int i = 0; i <ordre; i++)
+            for (int i = 0; i < ordre; i++)
             {
-               ListeAdjacence[i] = new List<int>();                 /// On crée une liste pour chaque élément du tableau
+                ListeAdjacence[i] = new List<int>();                 /// On crée une liste pour chaque élément du tableau
             }
 
             for (int i = 0; i < matrice_relation.GetLength(0); i++)
             {
 
-                ListeAdjacence[matrice_relation[i, 0]-1].Add(matrice_relation[i, 1]);       /// On ajoute à la Liste d'adjacence du nombre de la colonne de gauche celui de la colonne de droite de la matrice_relation
-                ListeAdjacence[matrice_relation[i, 1]-1].Add(matrice_relation[i, 0]);       /// On fait également l'inverse car le graphe est non-orienté donc les relations sont réciproques
+                ListeAdjacence[Convert.ToInt32(matrice_relation[i, 0]) - 1].Add(Convert.ToInt32(matrice_relation[i, 1]));       /// On ajoute à la Liste d'adjacence du nombre de la colonne de gauche celui de la colonne de droite de la matrice_relation
+                ListeAdjacence[Convert.ToInt32(matrice_relation[i, 1]) - 1].Add(Convert.ToInt32(matrice_relation[i, 0]));       /// On fait également l'inverse car le graphe est non-orienté donc les relations sont réciproques
             }
 
             Console.WriteLine("Liste d'adjacence :");               /// On l'affiche
@@ -115,19 +114,19 @@ namespace Problème_scientifique_informatique
                 Console.WriteLine();
             }
 
-         
-            return ListeAdjacence;
-                
-        }  
-       
 
-        public int[,] GenererMatriceAdjacence(int[,] matrice_relation, int ordre)
+            return ListeAdjacence;
+
+        }
+
+
+        public int[,] GenererMatriceAdjacence(T[,] matrice_relation, int ordre)
         {
             int[,] MatriceAdjacence = new int[ordre, ordre];                /// On crée une matrice d'adjacence de même largeur et hauteur correspondant à l'ordre du graphe
-            for(int i = 0; i<matrice_relation.GetLength(0); i++)
+            for (int i = 0; i < matrice_relation.GetLength(0); i++)
             {
-                MatriceAdjacence[matrice_relation[i, 0]-1, matrice_relation[i, 1]-1] = 1;     /// on rempli la matrice grâce à la matrice relation : on met des 1 lorsque les sommets i et j sont voisins.
-                MatriceAdjacence[matrice_relation[i, 1]-1, matrice_relation[i, 0]-1] = 1;
+                MatriceAdjacence[Convert.ToInt32(matrice_relation[i, 0]) - 1, Convert.ToInt32(matrice_relation[i, 1]) - 1] = 1;     /// on rempli la matrice grâce à la matrice relation : on met des 1 lorsque les sommets i et j sont voisins.
+                MatriceAdjacence[Convert.ToInt32(matrice_relation[i, 1]) - 1, Convert.ToInt32(matrice_relation[i, 0]) - 1] = 1;
             }
 
             Console.WriteLine("Matrice d'adjacence :");    /// On l'affiche
@@ -144,12 +143,12 @@ namespace Problème_scientifique_informatique
             return MatriceAdjacence;
         }
 
-        public List<int> ParcoursEnProfondeurDabord(Noeud[] noeuds)
+        public List<int> ParcoursEnProfondeurDabord(Noeud<T>[] noeuds, T[,]matrice_relation)
         {
             List<int> SommetsVisites = new List<int>();      /// On crée la liste des sommets visités qui sera retournée à la fin du programme
 
             int SommetDepart = Convert.ToInt32(Console.ReadLine());           /// L'utilisateur saisi le sommet de départ
-            while (SommetDepart>34 || SommetDepart<=0)
+            while (SommetDepart > matrice_relation.Length || SommetDepart <= 0)
             {
                 Console.WriteLine("Le sommet n'est pas valide : il faut saisir un sommet existant");
                 SommetDepart = Convert.ToInt32(Console.ReadLine());           /// l'utilisateur saisi le sommet de départ jusqu'à ce qu'il soit valide
@@ -157,16 +156,16 @@ namespace Problème_scientifique_informatique
 
 
             Stack<int> pile = new Stack<int>();           /// On créé la pile qui va stocker les sommets visités et on y ajoute le sommet de départ.
-            pile.Push(SommetDepart);                    
+            pile.Push(SommetDepart);
 
-            
+
             noeuds[SommetDepart - 1].couleur = "jaune";       /// Le sommet de départ passe en jaune
             int Sommet = SommetDepart;
 
             int AvantDernier = SommetDepart;                  /// On crée une variable qui stocke l'avant dernier sommet parcouru : cela sera utile pour étudier l'existence de cycles
             bool cycle = false;
             int[] tabcycle = new int[noeuds.Length];       /// le tableau de cycle crée permettra de renvoyer un exemple de cycle
-            int nbelements=0;
+            int nbelements = 0;
             int n = 0;
 
             while (pile.Count != 0)                         /// l'algorithme s'arrêtera quand la pile sera vide
@@ -176,41 +175,41 @@ namespace Problème_scientifique_informatique
                 {
                     if (noeuds[noeuds[Sommet - 1].listevoisins[i] - 1].couleur == "blanc")           ///Si le voisin regardé est blanc alors :
                     {
-                        AvantDernier = pile.Peek();                                                  
+                        AvantDernier = pile.Peek();
                         pile.Push(noeuds[Sommet - 1].listevoisins[i]);                               /// on l'ajoute à la pile
                         noeuds[noeuds[Sommet - 1].listevoisins[i] - 1].couleur = "jaune";            /// il devient jaune
                         Sommet = noeuds[Sommet - 1].listevoisins[i];                                 /// il devient le nouveau Sommet actuel
-                                        
+
                         break;                                                                       /// on sort de la boucle pour étudier ses voisins à lui et ainsi de suite
                     }
-                    else if (noeuds[noeuds[Sommet - 1].listevoisins[i] - 1].couleur == "jaune" || noeuds[noeuds[Sommet - 1].listevoisins[i] - 1]. couleur == "rouge")
+                    else if (noeuds[noeuds[Sommet - 1].listevoisins[i] - 1].couleur == "jaune" || noeuds[noeuds[Sommet - 1].listevoisins[i] - 1].couleur == "rouge")
                     {
                         /// Le "if" suivant correspond à l'étude de l'existence de cycles
-                        if (noeuds[noeuds[Sommet - 1].listevoisins[i] - 1].couleur == "jaune" && noeuds[Sommet - 1].listevoisins[i] != AvantDernier && cycle==false  && Sommet != AvantDernier)     
+                        if (noeuds[noeuds[Sommet - 1].listevoisins[i] - 1].couleur == "jaune" && noeuds[Sommet - 1].listevoisins[i] != AvantDernier && cycle == false && Sommet != AvantDernier)
                         {
-    
+
                             int[] tabpile = pile.ToArray();          /// On converti la pile en tableau pour pouvoir isoler le cycle
                             Array.Reverse(tabpile);                  /// Nous inversons ce tableau afin de ne pas avoir à décrémenter dans les boucles à suivre, mais plutôt incrémenter.
-                                
-                            for (int j=0; j<tabpile.Length; j++)               /// On parcours le tableau correspondant à la pile
+
+                            for (int j = 0; j < tabpile.Length; j++)               /// On parcours le tableau correspondant à la pile
                             {
-                                if(tabpile[j] == noeuds[Sommet - 1].listevoisins[i] && tabpile.Length - j>2)      /// On chercher l'indice j du sommet à partir duquel le cycle commence (soit le sommet correspondant au voisin étudié). De plus, un cycle doit contenir stricement plus de deux sommets.  
+                                if (tabpile[j] == noeuds[Sommet - 1].listevoisins[i] && tabpile.Length - j > 2)      /// On chercher l'indice j du sommet à partir duquel le cycle commence (soit le sommet correspondant au voisin étudié). De plus, un cycle doit contenir stricement plus de deux sommets.  
+                                {
+                                    nbelements = tabpile.Length - j;               /// le nombre d'éléments du cycle trouvé sera donc trouvé en calculant : taille de la pile - les chiffres précédent le sommet de début du cycle (ceux qui ne sont pas dans le cycle et qui sont au nombre de j)
+
+                                    cycle = true;
+                                    n = j;
+                                    for (int k = 0; k < tabpile.Length - j; k++)         /// On rempli le tableau autant de fois qu'il y a d'éléments dans le cycle (la fin est à tabpile.Length-j car la pile à été inversé donc les éléments non-souhaités sont à la fin du tableau)
                                     {
-                                        nbelements=tabpile.Length - j;               /// le nombre d'éléments du cycle trouvé sera donc trouvé en calculant : taille de la pile - les chiffres précédent le sommet de début du cycle (ceux qui ne sont pas dans le cycle et qui sont au nombre de j)
-                                               
-                                        cycle = true;
-                                        n = j;
-                                        for (int k = 0; k < tabpile.Length - j; k++)         /// On rempli le tableau autant de fois qu'il y a d'éléments dans le cycle (la fin est à tabpile.Length-j car la pile à été inversé donc les éléments non-souhaités sont à la fin du tableau)
-                                        {
-                                            tabcycle[k] = tabpile[n];      /// Le tableau du cycle est rempli avec les bons éléments du tableau de la pile.
-                                            n += 1;
-                                        }
-                                        break;          /// Lorsque le tableau est rempli et que nous avons un cycle comme exemple, nous sortons de la boucle et continuons l'algorithme.
-            
+                                        tabcycle[k] = tabpile[n];      /// Le tableau du cycle est rempli avec les bons éléments du tableau de la pile.
+                                        n += 1;
                                     }
+                                    break;          /// Lorsque le tableau est rempli et que nous avons un cycle comme exemple, nous sortons de la boucle et continuons l'algorithme.
+
+                                }
                             }
-                        }                  
-                                   
+                        }
+
                         if (i < (noeuds[Sommet - 1].listevoisins.Count - 1))
                         {
                             continue;                                   /// Si le sommet voisin est jaune ou rouge mais qu'il existe un ou plusieurs autres sommets voisins alors on continue;
@@ -229,21 +228,21 @@ namespace Problème_scientifique_informatique
                 }
             }
             Console.Write(SommetsVisites[0]);
-            for (int i=1; i<SommetsVisites.Count; i++)
+            for (int i = 1; i < SommetsVisites.Count; i++)
             {
                 Console.Write(" ; " + SommetsVisites[i]);               /// On affiche la liste des sommets visités, soit le chemin complet de parcours du graphe.
             }
             Console.WriteLine();
 
 
-        if (cycle == true)
-        {
-            Console.Write("Il existe un cycle, par exemple : ");
-            for (int i=0;i<nbelements;i++)
+            if (cycle == true)
+            {
+                Console.Write("Il existe un cycle, par exemple : ");
+                for (int i = 0; i < nbelements; i++)
                 {
                     Console.Write(tabcycle[i] + " ");         /// Si il y a un cycle, on affiche le tableau contenant l'exemple de cycle
                 }
-        }
+            }
             else { Console.WriteLine("Il n'existe pas de cycle"); }
 
 
@@ -269,9 +268,9 @@ namespace Problème_scientifique_informatique
                         connexite = true;
                         break;
                     }
-                        
+
                 }
-                if (connexite==false)
+                if (connexite == false)
                 {
                     break;                                     /// Si la connexité reste à false, alors au moins 1 sommet n'a pas été visité donc on sort de la première boucle et l'algorithme se termine : le graphe n'est pas connexe.
                 }
@@ -285,11 +284,11 @@ namespace Problème_scientifique_informatique
 
 
 
-        public List<int> ParcoursEnLargeurDabord(Noeud[] noeuds)
+        public List<int> ParcoursEnLargeurDabord(Noeud<T>[] noeuds, T[,] matrice_relation)
         {
             List<int> SommetsVisites2 = new List<int>();                            /// On initialise la liste de Sommetvisités du parcours en largeur
             int SommetDepart = Convert.ToInt32(Console.ReadLine());
-            while (SommetDepart > 34 || SommetDepart <= 0)
+            while (SommetDepart > matrice_relation.Length || SommetDepart <= 0)
             {
                 Console.WriteLine("Le sommet n'est pas valide : il faut saisir un sommet existant");          /// Comme précédemment, l'utilisateur saisi le sommet de départ jusqu'à ce qu'il soit valide
                 SommetDepart = Convert.ToInt32(Console.ReadLine());
@@ -303,20 +302,20 @@ namespace Problème_scientifique_informatique
 
             while (file.Count != 0)                       /// L'algorithme se terminera quand le queue sera vide.
             {
-                Sommet = file.Peek();                      
+                Sommet = file.Peek();
                 SommetsVisites2.Add(Sommet);
                 file.Dequeue();                                   /// On retire le premier sommet de la queue puis on met tous ses voisins blanc en jaune et on les ajoute dans la queue.
-                for (int i=0; i < noeuds[Sommet-1].listevoisins.Count; i++)
+                for (int i = 0; i < noeuds[Sommet - 1].listevoisins.Count; i++)
                 {
-                    if (noeuds[noeuds[Sommet - 1].listevoisins[i] - 1].couleur =="blanc")
+                    if (noeuds[noeuds[Sommet - 1].listevoisins[i] - 1].couleur == "blanc")
                     {
                         noeuds[noeuds[Sommet - 1].listevoisins[i] - 1].couleur = "jaune";
                         file.Enqueue(noeuds[Sommet - 1].listevoisins[i]);
-                    }                  
+                    }
                 }
 
                 noeuds[Sommet - 1].couleur = "rouge";         /// Le noeud dont tous les voisins sont passés jaune a été traité complètement : il passe en rouge.
-                
+
             }
 
             for (int i = 0; i < SommetsVisites2.Count; i++)
