529e20c0887233192dbdaa86e87d2a039eace250
Alexie Fournié <alexie.fournie@gmail.com>
2025-04-01 11:05:04 +0200

    Ajout du tableau nomStation et du sommaire

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace ProjetPSI
         {
             string fichier = "..\\net7.0\\MetroParis.xlsx";                /// On accède au fichier excel MetroParis contenant tous les liens du graphe
             int[,] matrice_relation = null;
+            string[] tableau_nomStation = null;
 
             if (!File.Exists(fichier))
             {
@@ -27,16 +28,18 @@ namespace ProjetPSI
                 ExcelPackage.License.SetNonCommercialPersonal("ProjetPSI");
                 using (var package = new ExcelPackage(fichierInfo))
                 {
-                    ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                    ExcelWorksheet worksheet1 = package.Workbook.Worksheets[1];
+                    ExcelWorksheet worksheet2 = package.Workbook.Worksheets[0];
 
-                    matrice_relation = new int[worksheet.Dimension.End.Row - 2, 3];
+                    matrice_relation = new int[worksheet1.Dimension.End.Row - 2, 4];
+                    tableau_nomStation = new string[worksheet2.Dimension.End.Row - 1];
 
-                    for (int ligne = 2; ligne < worksheet.Dimension.End.Row; ligne++)
+                    for (int ligne = 2; ligne < worksheet1.Dimension.End.Row; ligne++)
                     {
-                        int idStation = Convert.ToInt32(worksheet.Cells[ligne, 1].Value);
-                        int idVoisin = Convert.ToInt32(worksheet.Cells[ligne, 3].Value);
-                        int doubleSens = Convert.ToInt32(worksheet.Cells[ligne, 4].Value);
-                        int poids = Convert.ToInt32(worksheet.Cells[ligne, 5].Value);
+                        int idStation = Convert.ToInt32(worksheet1.Cells[ligne, 1].Value);
+                        int idVoisin = Convert.ToInt32(worksheet1.Cells[ligne, 3].Value);
+                        int doubleSens = Convert.ToInt32(worksheet1.Cells[ligne, 4].Value);
+                        int poids = Convert.ToInt32(worksheet1.Cells[ligne, 5].Value);
 
                         if (idStation != null && idVoisin!= null)
                         {
@@ -46,62 +49,37 @@ namespace ProjetPSI
                             matrice_relation[ligne - 2, 3] = poids;
                         }
                     }
-                }
-            }
 
-            for (int i = 0; i < matrice_relation.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrice_relation.GetLength(1); j++)
-                {
-                    Console.Write(matrice_relation[i, j] + " ");
+                    for (int ligne=2; ligne<=329; ligne++)
+                    {
+                        string nomStation = Convert.ToString(worksheet2.Cells[ligne, 3].Value);
+                        tableau_nomStation[ligne-2] = nomStation;
+                    }
                 }
-                Console.WriteLine();
+            }       
+
+
+            static void SommaireMetro (string[] tableau_nomStation )
+            {
+                Console.WriteLine("Bienvenue sur le SOMMAIRE des stations de métro. Chaque station de métro est associé à son identifiant.");
+                for (int i = 1; i <= tableau_nomStation.Length; i++)
+                {
+                    Console.WriteLine(i + " : " + tableau_nomStation[i - 1]);
+                }
             }
 
             Graphe<int> GrapheMetro = new Graphe<int>(matrice_relation);
 
             int ordre = GrapheMetro.OrdreDuGraphe(matrice_relation);
+            int taille = GrapheMetro.TailleDuGraphe(matrice_relation);
+            List<int>[] ListeAdjacence = GrapheMetro.GenererListeAdjacence(matrice_relation, ordre);
+            bool Oriente = GrapheMetro.GrapheOriente(ListeAdjacence);
 
-            GrapheMetro.GenererListeAdjacence(matrice_relation, ordre);
+            SommaireMetro(tableau_nomStation);
 
+            // Visuel VisuelGraphe = new Visuel(GrapheMetro.GenererListeAdjacence(matrice_relation, ordre));
 
-            Noeud<int> [] noeuds = new Noeud<int> [ordre];          /// On initialise un tableau noeuds de Noeud.
-            for (int i = 0; i < ordre; i++)
-            {
-                noeuds[i] = new Noeud<int> (i, "blanc", ListeAdjacence[i]);            /// Tous les noeuds de graphe prennent en paramètre un numéro i, la couleur blanche et une liste de voisins correspondant à la liste d'adjacence du sommet étudié.
-            }
-    
-
-            Visuel VisuelGraphe = new Visuel(GrapheMetro.GenererListeAdjacence(matrice_relation, ordre));
-
-            VisuelGraphe.DessinerGraphe();
-
-
-            Lien<int>[,] liens = new Lien<int>[ordre, ordre];                     /// On initialise une matrice de lien qui contient autant de lien qu'il y en a dans la matrice relation et qui relie un sommet de départ a à un sommet d'arrivée b compris dans l'ordre du graphe
-            for(int a=0 ; a <ordre; a++)                                                       
-            {
-                for (int b=0; b<ordre; b++)   
-                {
-                    if (ListeAdjacence[a].Contains(b+1))                    /// Pour tout a et b, si le sommet b est un voisin de a, alors le lien existe
-                    {
-                        for (int h=0; h<matrice_relation.GetLength(0); h++)
-                        {
-                            if (matrice_relation[h, 0] == a+1 && matrice_relation[h, 1] == b+1)          /// on parcours la matrice relation à la recherche de la ligne correspondant au lien a-b avec donc le terme de la première colonne égale à a et celui de la deuxième égale à b
-                            {
-                                int aa = h;                                                         /// lorsqu'on trouve cette ligne, on note son indice
-                                liens[a, b] = new Lien<int>(noeuds[matrice_relation[aa, 0] - 1], noeuds[matrice_relation[aa, 1] - 1], matrice_relation[aa, 3]);              /// on peut maintenant associée à un lien un NOEUD de départ, un NOEUD d'arrivé, et le poids correspondant à ce lien.
-                                break; 
-                            }
-                        }
-            
-                    }
-        
-                }
-            }
-
-            Console.ReadKey();
-
-            
+            // VisuelGraphe.DessinerGraphe();
         }
     }
 
