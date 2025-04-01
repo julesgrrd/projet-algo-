bc0448ede854f4d6d872d06fa25196c9ce3dce88
orianeefllx <oriane.filloux@edu.devinci.fr>
2025-04-01 18:54:12 +0200

    update du tableau de noeud (modifier aussi dans le constructeur)

--- Program.cs
+++ Program.cs
@@ -80,7 +80,7 @@ namespace ProjetPSI
             Noeud<int> [] noeuds = new Noeud<int> [ordre];          /// On initialise un tableau noeuds de Noeud.
             for (int i = 0; i < ordre; i++)
             {
-                noeuds[i] = new Noeud<int> (i, "blanc", ListeAdjacence[i]);            /// Tous les noeuds de graphe prennent en paramètre un numéro i, la couleur blanche et une liste de voisins correspondant à la liste d'adjacence du sommet étudié.
+                noeuds[i] = new Noeud<int> (i+1, tableau_nomStation[i], "blanc", ListeAdjacence[i]);            /// Tous les noeuds de graphe prennent en paramètre un numéro i, la couleur blanche et une liste de voisins correspondant à la liste d'adjacence du sommet étudié.
             }
 
             
@@ -110,6 +110,8 @@ namespace ProjetPSI
         
                 }
             }
+
+            graphe.AlgorithmeDijkstra(noeuds, liens, ordre);
 
 
             
